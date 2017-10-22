413f2fe4b24609b5fa70e29f21c9ff7c26a3d9a4
Kartik Verma <kvs1297@gmail.com>
2017-10-22 23:34:35 +0530

    added start and gameover screen

--- src/gameloop.py
+++ src/gameloop.py
@@ -13,12 +13,6 @@ class MainGameLoop:
 		self.score = 0
 		self.runGame = True
 		self.game_screen()
-		self.bird = Bird.Bird(self.window, self.fpsClock, self.FPS, Coords.Coords((100, int(480/2))))
-		self.curTime = self.prevTime = pg.time.get_ticks()
-		self.time_diff = 3500
-		self.grass = Grass.Grass(self.window, Coords.Coords((640, 480)))
-		self.cloud = Cloud.Cloud(self.window, Coords.Coords((640, 480)))
-		self.pipes = [Pipe.Pipe(self.window, Coords.Coords((640, 480)))]
 
 	def game_screen(self, size = (640, 480)):
 		self.window = pg.display.set_mode(size)
@@ -38,7 +32,24 @@ class MainGameLoop:
 		pg.quit()
 		sys.exit()
 
+	def clear_data(self):
+		self.score = 0
+		self.bird = Bird.Bird(self.window, self.fpsClock, self.FPS, Coords.Coords((100, int(480/2))))
+		self.curTime = self.prevTime = pg.time.get_ticks()
+		self.time_diff = 3500
+		self.grass = Grass.Grass(self.window, Coords.Coords((640, 480)))
+		self.cloud = Cloud.Cloud(self.window, Coords.Coords((640, 480)))
+		self.pipes = [Pipe.Pipe(self.window, Coords.Coords((640, 480)))]
+
+
 	def run(self):
+		self.startScreen()
+		while True:
+			self.clear_data()
+			self.mainGame()
+			self.gameOver()
+
+	def mainGame(self):
 		self.runGame = True
 		while self.runGame == True:
 			if len(self.pipes) > 0 and self.pipes[0].visible == False:
@@ -75,4 +86,52 @@ class MainGameLoop:
 			return True
 		else:
 		 return False
+
+	def startScreen(self):
+		startCloud = pg.transform.scale(pg.image.load('objects/startscreen/cloud.jpg'), (640, 480))
+		startbird = pg.transform.scale(pg.image.load("objects/startscreen/bird.png"), (64, 64))
+		startPlayButton = pg.transform.scale(pg.image.load('objects/startscreen/play.png'),(128, 128))
+		c = None
+		startFlappyBird = TypeFaces.Draws(dispSurf = self.window, typeface="./fonts/CHLORINR.TTF", string="FLaPpY Wings", fontsize = 72, coords = Coords.Coords((int(640/2), int(480/5))), use_center = True, color_left=pg.Color(0, 120, 0))
+		startCloudRect = startCloud.get_rect()
+		startCloudRect.top = startCloudRect.left = 0
+		startbirdRect = startbird.get_rect()
+		startbirdRect.centerx = 60
+		startbirdRect.centery = int(480/2)
+		startPlayButtonRect = startPlayButton.get_rect()
+		startPlayButtonRect.centerx = int(640/2)
+		startPlayButtonRect.centery = int(480/2)
+		mouseClicked = False
+		while True:
+			self.window.fill((0, 0, 0))
+			self.window.blit(startCloud, startCloudRect)
+			self.window.blit(startbird, startbirdRect)
+			self.window.blit(startPlayButton, startPlayButtonRect)
+			startFlappyBird.drawFont()
+			self.checkForQuit()
+			for event in pg.event.get():
+				if event.type == pglcl.MOUSEBUTTONUP:
+					c = Coords.Coords(event.pos)
+					mouseClicked = True 
+			if mouseClicked == True and startPlayButtonRect.colliderect(pg.Rect(c.x, c.y, startPlayButtonRect.width, startPlayButtonRect.height)) == True:
+				return
+			pg.display.update()
+			self.fpsClock.tick(self.FPS)
+
+
+	def gameOver(self):
+		GameOverSurface = pg.Surface((640, 480))
+		GameOverSurface = GameOverSurface.convert_alpha()
+		GameOverSurface.fill((0, 0, 0, 50))
+		gameOverGameOver = TypeFaces.Draws(dispSurf = self.window, typeface="./fonts/CHLORINR.TTF", string="Game Over", fontsize = 72, coords = Coords.Coords((int(640/2), int(480/2))), use_center = True, color_left=pg.Color(0, 120, 0)) 
+		self.window.blit(GameOverSurface, (0, 0))
+		gameOverGameOver.drawFont()
+		pg.display.update()
+		while True:
+			self.checkForQuit()
+			for event in pg.event.get():
+				if event.type == pglcl.KEYUP:
+					return
+
+
 
